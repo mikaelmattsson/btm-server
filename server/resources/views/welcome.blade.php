0469e0a26fdf0e08283ffc651a5e2d061c815553
Mikael Mattsson <mikael.p.mattsson@gmail.com>
2017-08-15 20:35:23 +0200

    Update title

--- server/resources/views/welcome.blade.php
+++ server/resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Battlemage Training</title>
 
     <link href="https://fonts.googleapis.com/css?family=Bangers|Patua+One" rel="stylesheet">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
